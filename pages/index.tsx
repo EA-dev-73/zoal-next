ce57d3db85a4c3dc4e6ff42b279daff24da88355
TMN <t.martin@adsoftware.fr>
2022-03-14 17:26:09 +0100

    style: image + title

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
 import type { NextPage } from "next";
+import Image from "next/image";
 import { Layout } from "../components/Layout";
+import zolaImage from "../public/zola.jpg";
 
 const Home: NextPage = () => {
   return (
-    <Layout pageTitle="Accueil">
+    <Layout pageTitle="Zoal">
       <h1>Présentation</h1>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ab, tempore
@@ -11,6 +13,7 @@ const Home: NextPage = () => {
         saepe libero aliquid qui autem corporis cupiditate reiciendis corrupti
         voluptatem totam!
       </p>
+      <Image src={zolaImage} alt="zola-image" />
     </Layout>
   );
 };
