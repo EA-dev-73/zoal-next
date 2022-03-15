d6b9047a7c285d8225c1d00fcdb106c44e201a26
TMN <t.martin@adsoftware.fr>
2022-03-15 19:25:18 +0100

    fix: remove only one product in cart

--- utils/localStorageHelpers.ts
+++ utils/localStorageHelpers.ts
@@ -23,16 +23,8 @@ export const addProductIdToCart = (itemId: Product["id"]) => {
 export const removeItemFromCart = (selectedItemId: Product["id"]): void => {
   if (typeof window !== "undefined") {
     const cartContent = getCartContentFromLocalStorage();
-    const itemToRemove = cartContent.find((x) => x === selectedItemId);
-    if (!itemToRemove) {
-      console.error(
-        "L'item que l'on souhaite supprimé du cart n'a pas été trouvé 🤔"
-      );
-      return;
-    }
-    localStorage.setItem(
-      LOCAL_STORAGE_CART_KEY,
-      JSON.stringify(cartContent.filter((x) => x !== itemToRemove))
-    );
+    const idxToRemove = cartContent.findIndex((x) => x === selectedItemId);
+    cartContent.splice(idxToRemove, 1);
+    localStorage.setItem(LOCAL_STORAGE_CART_KEY, JSON.stringify(cartContent));
   }
 };
