25800dc771345971f356ca6ed51f2f6341b6d476
TMN <t.martin@adsoftware.fr>
2022-09-01 15:14:57 +0200

    feat: preview des images + zoom dans table admin

--- README.md
+++ README.md
@@ -5,11 +5,8 @@
 ## Todo list
 
 - [ ] admin => bug suppresion d'une image
-- [ ] pouvoir update les images d'un product type sans editer le reste du product type
 - [ ] fix suppression des images du bucket quand un productType est supprimé
-- [ ] mise a jour de laffichage des images apres suppresion d'une image
 - [ ] style
 - [ ] améliorer le référencement naturel
-- [ ] utiliser une lib pour meilleure gestion des images coté admin (ex: cliquer pour agrandir)
 - [ ] sécuriser stripe (var d'env server sur vercel OK ??) + supabase (RLS UPDATE/INSERT products, validatedOrder)
 - [ ] setter les var d'envs de prod sur vercel (supabase, stripe, stripe webhooks)
--- components/AdminProductsTable/DisplayCurrentProductPictures.tsx
+++ components/AdminProductsTable/DisplayCurrentProductPictures.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
 import { useDeleteImage } from "../../api/images";
 import { ProductType, ProductTypeWithImages } from "../../types";
 import { displayToast } from "../../utils/displayToast";
+import dynamic from "next/dynamic";
+import ImageViewer from "react-simple-image-viewer";
 
 type Props = {
   productTypeId?: ProductType["id"];
@@ -15,7 +17,10 @@ export const DisplayCurrentProductImages = ({
   imagesUrls = [],
   isEdit = true,
 }: Props) => {
+  const Viewer = dynamic(() => import("react-viewer"), { ssr: false });
   const { mutate: deleteImage } = useDeleteImage();
+  const [showPreview, setShowPreview] = useState(false);
+  const [activeIndex, setActiveIndex] = useState(0);
   const handleDelete = async (imageUrl: string) => {
     if (!productTypeId) {
       displayToast({
@@ -31,17 +36,34 @@ export const DisplayCurrentProductImages = ({
     });
   };
 
-  console.log(imagesUrls?.length, isEdit);
   return (
     <div className="d-flex flex-wrap justify-content-around">
-      {imagesUrls.map((imageUrl) => (
+      {imagesUrls.map((imageUrl, idx) => (
         <div key={imageUrl} className="d-flex flex-column">
           <Image
             src={imageUrl}
             width={100}
             height={100}
             alt={`Image ${imageUrl}`}
+            onClick={() => {
+              setShowPreview(true);
+              setActiveIndex(idx);
+            }}
+            className="img-preview"
           />
+
+          {showPreview && (
+            <ImageViewer
+              src={imagesUrls}
+              currentIndex={activeIndex}
+              onClose={() => setShowPreview(false)}
+              disableScroll={false}
+              backgroundStyle={{
+                backgroundColor: "rgba(0,0,0,0.9)",
+              }}
+              closeOnClickOutside
+            />
+          )}
           {isEdit && (
             <button
               role="button"
