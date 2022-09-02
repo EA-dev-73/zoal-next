279679b9ff34969deb66ccb63bf4b40fc2ab0a37
TMN <t.martin@adsoftware.fr>
2022-09-02 21:16:04 +0200

    feat: ajout nom de la categorie dans les listes

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -1,7 +1,7 @@
 import { orderBy } from "lodash";
 import { GetStaticProps } from "next";
 import { useRouter } from "next/router";
-import { useEffect, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { useCategories } from "../api/category";
 import { useProductTypesWithImages } from "../api/products/product-type";
 import { Layout } from "../components/Layout";
@@ -15,38 +15,46 @@ export default function Shop() {
 
   const { productTypesWithImages, isLoading } = useProductTypesWithImages();
 
+  const categoryToFilter = useMemo(
+    () =>
+      (categories || []).find(
+        (x) =>
+          x.name?.toLowerCase() === Object.keys(router.query)[0]?.toLowerCase()
+      ),
+    [categories, router.query]
+  );
+
   useEffect(() => {
     if (!router.query) {
       setFilter(null);
       return;
     }
-    const catToFilter = (categories || []).find(
-      (x) =>
-        x.name?.toLowerCase() === Object.keys(router.query)[0]?.toLowerCase()
-    );
 
-    if (!catToFilter) {
+    if (!categoryToFilter) {
       setFilter(null);
       return;
     }
 
-    setFilter(catToFilter.id);
-  }, [router.query, categories, router]);
+    setFilter(categoryToFilter.id);
+  }, [categoryToFilter, router.query]);
 
   return (
     <Layout>
+      {categoryToFilter ? <h2>{categoryToFilter.name}</h2> : ""}
       <div className="d-flex flex-wrap justify-content-between mx-5">
         {isLoading ? (
           <p>chargement des produits... </p>
         ) : (
-          orderBy(productTypesWithImages || [], "createdAt", "desc")
-            .filter((productType) =>
-              filter ? productType.productCategory.id === filter : true
-            )
+          <>
+            {orderBy(productTypesWithImages || [], "createdAt", "desc")
+              .filter((productType) =>
+                filter ? productType.productCategory.id === filter : true
+              )
 
-            .map((productType) => (
-              <ProductCard key={productType.id} productType={productType} />
-            ))
+              .map((productType) => (
+                <ProductCard key={productType.id} productType={productType} />
+              ))}
+          </>
         )}
       </div>
     </Layout>
